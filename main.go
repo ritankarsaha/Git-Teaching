955b443ac0efee38724b28aeba24c49b08b41759
RITANKAR SAHA <ritankar.saha786@gmail.com>
2026-05-13 15:25:21 +0530

    looping in golang
    maps in golang
    
    added space

--- main.go
+++ main.go
@@ -7,4 +7,27 @@ func main(){
 
 
 	fmt.Println("Hello, World!")
+
+
+
+	//looping in golang 
+	for i := 0; i < 5; i++ {
+		fmt.Printf("Iteration %d\n", i)
+	}
+
+
+
+
+
+	// maps in golang
+	person := make(map[string]string)
+	person["name"] = "John"
+	person["age"] = "30"
+	person["city"] = "New York"
+
+	fmt.Println("Person:", person)
+
+
+	
+
 }
